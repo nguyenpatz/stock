dfdbf5841970aa5b76b88121d45b0168259164e9
nguyenpatz <nguyenpatzz@gmail.com>
2022-11-14 10:50:16 +0700

    add some seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('user')->insert(
+        [
+            ['name' => 'admin1', 'password' => 'admin123', 'perm_read' => 1, 'perm_write' => 1, 'perm_create' => 1, 'perm_unlink' => 1],
+            ['name' => 'admin2', 'password' => 'admin123', 'perm_read' => 1, 'perm_write' => 1, 'perm_create' => 1, 'perm_unlink' => 1],
+        ]);
+    }
+}
